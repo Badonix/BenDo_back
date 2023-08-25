af6985fcbd79db0273adaa13a27d85bf35227ceb
Nick <nikoloz.danelia@redberry.ge>
2023-08-25 23:13:50 +0400

    feat: casting profession and languages to array

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,5 +46,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        "profession" => "array",
+        "languages" => "array"
     ];
 }
